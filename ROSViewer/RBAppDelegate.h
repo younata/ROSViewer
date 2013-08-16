357abf1a3147bd577b2e9f0bed06d16cbdecd2c9
Rachel Brindle <you@subluminal.net>
2013-08-16 16:49:17 -0400

    Initial Commit

---
+++ ROSViewer/RBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBAppDelegate.h
+//  ROSViewer
+//
+//  Created by Rachel Brindle on 8/16/13.
+//  Copyright (c) 2013 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ROSViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ROSViewer
+//
+//  Created by Rachel Brindle on 8/16/13.
+//  Copyright (c) 2013 Rachel Brindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RBAppDelegate class]));
+    }
+}
